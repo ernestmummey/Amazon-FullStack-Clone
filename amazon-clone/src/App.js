fe75c7fa2b8b647c93d0956347f8b2023e70bddb
Ernest Mummey <ernest.j.mummey@gmail.com>
2021-07-13 10:29:17 -0400

    imported the Home.jsx

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Views/Header.jsx'
+import Home from './Views/Home.jsx'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Home/>
     </div>
   );
 }
