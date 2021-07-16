32bf1e822fa0095ec7300196e8fc1cf1eccbc948
Ernest Mummey <ernest.j.mummey@gmail.com>
2021-07-15 22:06:35 -0400

    Created a SingleProduct.jsx

---
+++ amazon-clone/src/Components/SingleProduct.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Style/SingleProduct.css'
+
+const SingleProduct = props => {
+    return (
+        <div>
+
+        </div>
+    )
+};
+
+export default SingleProduct;
