a9e68e9503f79b87c3f21bc92af06ec0cd0c29e3
Ernest Mummey <ernest.j.mummey@gmail.com>
2021-07-28 10:56:42 -0400

    Creating a grid for the home page

--- amazon-clone/src/Views/Home.jsx
+++ amazon-clone/src/Views/Home.jsx
@@ -13,25 +13,31 @@ const Home=(props)=>{
                     src='https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/ZmVhMDM4ZmYt/ZmVhMDM4ZmYt-OWQ2NjJjNmMt-w3000._CB668936041_.jpg' 
                     alt=''
                 />
-                <Grid container className="home__productsContainer" spacing={10}>
-                <Movie
-                    title=' "The Pursuit of Love" '
-                    image='https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/TV/TPOL_S1_DashboardCard_379x304_PRE_Final_noLocale_DOT0084._SY304_CB666234560_.jpg'
-                    aTag='Watch trailer on Prime Video'
-                />
-                < OverStock 
-                    title='Discounted overstock items'
-                    watchImage='https://images-na.ssl-images-amazon.com/images/G/01/AmazonServices/Site/US/Product/FBA/Outlet/Merchandising/Outlet_GW_W_186x116_Evergreen_Jul_2021._SY116_CB665657905_.jpg'
-                    watchImageText='Watch'
-                    shoesImage='https://images-na.ssl-images-amazon.com/images/G/01/AmazonServices/Site/US/Product/FBA/Outlet/Merchandising/Outlet_GW_SH_186x116_Evergreen_Jul_2021_v2._SY116_CB664188218_.jpg'
-                    shoesImageText='Shoes'
-                    pcImage='https://images-na.ssl-images-amazon.com/images/G/01/AmazonServices/Site/US/Product/FBA/Outlet/Merchandising/Outlet_GW_PC_186x116_Evergreen_Jul_2021._SY116_CB665657905_.jpg'
-                    pcImageText='PC'
-                    lawnImage='https://images-na.ssl-images-amazon.com/images/G/01/AmazonServices/Site/US/Product/FBA/Outlet/Merchandising/Outlet_GW_PLG_186x116_Evergreen_Jul_2021._SY116_CB665657905_.jpg'
-                    lawnImageText='Lawn & Garden'
-                    aLink='See more in Outlet'
-                />
-                </Grid>
+                <div className="home__productsContainer">
+                    <Grid container spacing={2} justifyContent="center">
+                        <Grid item xs={3} justifyContent="center">
+                            <Movie
+                                title=' "The Pursuit of Love" '
+                                image='https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/TV/TPOL_S1_DashboardCard_379x304_PRE_Final_noLocale_DOT0084._SY304_CB666234560_.jpg'
+                                aTag='Watch trailer on Prime Video'
+                            />
+                        </Grid>
+                        <Grid item xs={3}>
+                            < OverStock 
+                                title='Discounted overstock items'
+                                watchImage='https://images-na.ssl-images-amazon.com/images/G/01/AmazonServices/Site/US/Product/FBA/Outlet/Merchandising/Outlet_GW_W_186x116_Evergreen_Jul_2021._SY116_CB665657905_.jpg'
+                                watchImageText='Watch'
+                                shoesImage='https://images-na.ssl-images-amazon.com/images/G/01/AmazonServices/Site/US/Product/FBA/Outlet/Merchandising/Outlet_GW_SH_186x116_Evergreen_Jul_2021_v2._SY116_CB664188218_.jpg'
+                                shoesImageText='Shoes'
+                                pcImage='https://images-na.ssl-images-amazon.com/images/G/01/AmazonServices/Site/US/Product/FBA/Outlet/Merchandising/Outlet_GW_PC_186x116_Evergreen_Jul_2021._SY116_CB665657905_.jpg'
+                                pcImageText='PC'
+                                lawnImage='https://images-na.ssl-images-amazon.com/images/G/01/AmazonServices/Site/US/Product/FBA/Outlet/Merchandising/Outlet_GW_PLG_186x116_Evergreen_Jul_2021._SY116_CB665657905_.jpg'
+                                lawnImageText='Lawn & Garden'
+                                aLink='See more in Outlet'
+                            />
+                        </Grid>
+                    </Grid>
+                </div>
             </div>
         </div>
         
